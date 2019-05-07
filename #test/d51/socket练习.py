cdfeffdf45b986e9156347e1f6223dd0ae0becac
unknown <benjamin142857@outlook.com>
2019-05-07 20:03:16 +0800

    201905072003

--- #test/d51/socket练习.py
+++ #test/d51/socket练习.py
@@ -14,22 +14,24 @@ class MySocket:
         self.sk = socket.socket()
         self.sk.bind((ip, host))
         self.sk.listen()
-        self.get = ''
-        self.url = '/'
+        self.req_info = ''
+        self.req_url = '/'
+        self.url = []
         self.conn = None
         self.address = None
 
+
     def connection(self):
         self.conn, self.address = self.sk.accept()
-        self.get = self.conn.recv(512).decode('utf-8')
-        self.url = re.search('GET /.* HTTP/1.1', self.get).group().split(' ')[1]
+        self.req_info = self.conn.recv(512).decode('utf-8')
+        self.req_url = re.search('GET /.* HTTP/1.1', self.req_info).group().split(' ')[1]
 
-        print('{} {}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), self.url))
+        print('{} {}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), self.req_url))
 
-        if self.url == '/':
-            self.response('socket.html', status=200)
-        elif self.url == '/1.css':
-            self.response('1.css', status=200)
+        for url in self.url:
+            if self.req_url == url[0]:
+                url[1](self.response)
+                break
         else:
             self.response('404.html', status=404)
 
@@ -40,13 +42,23 @@ class MySocket:
             ret = f.read()
             f.close()
 
-        self.conn.send(bytes('HTTP/1.1 {} ok\r\n\r\n{}'.format(str(status), ret), encoding='utf-8'))
+        self.conn.send(bytes('HTTP/1.1 {} fuckyou\r\nserver: benjamin142857\r\nbefucker: alex\r\n\r\n{}'.format(str(status), ret), encoding='utf-8'))
 
 
 
 if __name__ == '__main__':
     mysk = MySocket('127.0.0.1', 9000)
 
+    def home(response):
+        response('socket.html', status=200)
+
+    def fun1(response):
+        response('f1.html', status=200)
+
+    mysk.url = [
+        ('/', home),
+        ('/fdf', fun1),
+    ]
     while True:
         mysk.connection()
 
--- Web_Django/urls.py
+++ Web_Django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01 import views as app01
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', app01.login),
 ]
